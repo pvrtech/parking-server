d80d00209e03b02e91931023b142abe29e5d6b4e
pvrtech2014 <pvrtech2014@gmail.com>
2020-03-07 15:57:51 +0530

    Parking schema created

--- app/models/parking.js
+++ app/models/parking.js
@@ -3,13 +3,13 @@ var mongoose = require('mongoose');
 var bcrypt   = require('bcrypt-nodejs');
 
 // define the schema for our dress model
-var dressSchema = mongoose.Schema({
+var parkingSchema = mongoose.Schema({
     name         : String,
     type         : Array,
     userid       : String,
     image        : Object,
-    events       : Array
+    location     : Array
 });
 
 // create the model for dresss and expose it to our app
-module.exports = mongoose.model('Dress', dressSchema);
+module.exports = mongoose.model('Parking', parkingSchema);
