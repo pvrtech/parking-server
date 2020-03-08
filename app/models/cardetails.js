ff8f3268ad3164a2d26381a9e8c9bb080779f504
PVR Tech <pvrtech2014@gmail.com>
2020-03-08 18:08:23 +0530

    Basic changes

--- app/models/cardetails.js
+++ app/models/cardetails.js
@@ -1,6 +1,5 @@
 // load the things we need
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt-nodejs');
 
 // define the schema for our Car Details model
 var carDetailsSchema = mongoose.Schema({
